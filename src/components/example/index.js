a52b2fb82b3f7f1b53f3835aa724f5359bbd9406
c0decisco <curavcy@gmail.comgit config --global user.email curavcy@gmail.comgit config --global user.email curavcy@gmail.com>
2021-05-13 14:21:26 -0700

    Added CSS Loader

--- src/components/example/index.js
+++ src/components/example/index.js
@@ -1,22 +1,21 @@
-import React from 'react'
-import Carousel from '../../../lib/Carousel';
-require('../../global.css');
+import React from "react";
+import Carousel from "../../../lib/Carousel";
 
 const settings = {
   loop: false,
   responsive: {
     320: {
-      gutter: 10
+      gutter: 10,
     },
     520: {
-      gutter: 20
+      gutter: 20,
     },
     768: {
-      gutter: 20
+      gutter: 20,
     },
     992: {
-      gutter: 20
-    }
+      gutter: 20,
+    },
   },
   mouseDrag: true,
   controls: false,
@@ -24,23 +23,23 @@ const settings = {
   speed: 600,
   touch: true,
   autoWidth: true,
-  viewportMax: true
+  viewportMax: true,
 };
 
 export default function SliderExample() {
   return (
     <div>
-      <Carousel settings={ settings }>
-        <img src='https://placekitten.com/200/300'/>
-        <img src='https://placekitten.com/300/300'/>
-        <img src='https://placekitten.com/400/300'/>
-        <img src='https://placekitten.com/200/300'/>
-        <img src='https://placekitten.com/300/300'/>
-        <img src='https://placekitten.com/400/300'/>
-        <img src='https://placekitten.com/200/300'/>
-        <img src='https://placekitten.com/300/300'/>
-        <img src='https://placekitten.com/400/300'/>
+      <Carousel settings={settings}>
+        <img src="https://placekitten.com/200/300" />
+        <img src="https://placekitten.com/300/300" />
+        <img src="https://placekitten.com/400/300" />
+        <img src="https://placekitten.com/200/300" />
+        <img src="https://placekitten.com/300/300" />
+        <img src="https://placekitten.com/400/300" />
+        <img src="https://placekitten.com/200/300" />
+        <img src="https://placekitten.com/300/300" />
+        <img src="https://placekitten.com/400/300" />
       </Carousel>
     </div>
-  )
+  );
 }
